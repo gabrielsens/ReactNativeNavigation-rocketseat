d9f051d54a5fc55c8d5e0eff36c30a2da7777310
Gabriel Sens <88062520+gabrielsens@users.noreply.github.com>
2023-06-13 13:28:52 +0000

    Adicionando os três tipos de navegação

---
+++ src/routes/tab.routes.tsx
@@ -0,0 +1,25 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from '@expo/vector-icons';
+import Feed from '../screns/Feed';
+import New from '../screns/New';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabRoutes() {
+    return (
+        <Tab.Navigator screenOptions={{
+            headerShown: false,
+        }}>
+            <Tab.Screen 
+                name='feed' 
+                component={Feed} options={{ 
+                tabBarIcon: ({ color, size }) => <Feather name='home' color={color} size={size} />,
+                tabBarLabel: 'Feed'
+            }} /> 
+            <Tab.Screen name='new' component={New} options={{
+                tabBarIcon: ({ color, size}) => <Feather name='plus' color={color} size={size} />,
+                tabBarLabel: 'Novo'
+            }} /> 
+        </Tab.Navigator>
+    );
+}
